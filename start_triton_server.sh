be7ec028373e64e636acae7204b15e1d0cf79f04
Dan Walkes <danwalkes@trellis-logic.com>
2025-06-04 13:38:02 -0600

    Switch from peoplenet_transformer to peoplenet
    Using the peoplenet config from [1]
    
    1: https://github.com/NVIDIA-AI-IOT/deepstream_tao_apps/tree/master/configs/triton-grpc/peoplenet_tao

--- start_triton_server.sh
+++ start_triton_server.sh
@@ -2,4 +2,4 @@
 set -e
 source $(dirname $0)/environment.sh
 
-/opt/tritonserver/bin/tritonserver --model-repository $TRITON_REPO_DIR --model-control-mode=explicit --load-model $MODEL_NAME --http-port=8000 --grpc-port=8001 --metrics-port=8002
+/opt/tritonserver/bin/tritonserver --model-repository $TRITON_REPO_DIR --model-control-mode=explicit --load-model $MODEL_NAME --http-port=8000 --grpc-port=10001 --metrics-port=8002
