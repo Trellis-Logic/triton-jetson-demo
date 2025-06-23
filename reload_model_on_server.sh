dcbdee43bec7f66d170b3fb0e51b75c89ce3d9ee
Dan Walkes <danwalkes@trellis-logic.com>
2025-06-23 11:24:39 -0600

    Use wget instead of curl for reload
    Works with busybox wget and doesn't require curl in the image

--- reload_model_on_server.sh
+++ reload_model_on_server.sh
@@ -2,4 +2,7 @@
 #!/bin/bash
 source $(dirname $0)/environment.sh
 
- curl -X POST localhost:8000/v2/repository/models/$MODEL_NAME/load
+wget -qO- \
+ --header="Content-Type: application/json" \
+ --post-data="" \
+ localhost:8000/v2/repository/models/$MODEL_NAME/load
